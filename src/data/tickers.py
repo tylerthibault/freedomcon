35de1722c8f9e2040bb9b82cf4cfa402e5f97caf
Tyler <tyler.t@GCC-TylerT.local>
2026-04-08 21:26:12 -0700

    adding procfile

---
+++ src/data/tickers.py
@@ -0,0 +1,13 @@
+ticketer1 = [
+    "WA Belongs to Jesus, Not the Democrats",
+    "Flip Washington",
+    "Make WA american Again",
+    "This father's day weekend become a founding father of a new revolution",
+    "An American Revelotion founded america. An American reformation will save it.",
+    "An American congress of christian men",
+    "250th anniversary of the united states"
+]
+
+ticketers = {
+    "ticketer1": ticketer1,
+}
