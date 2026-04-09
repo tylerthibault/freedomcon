a8f7e3bae0cf9bc64d8c4a60a74d3a45ec16c0cf
Tyler <tyler.t@GCC-TylerT.local>
2026-04-09 12:49:40 -0700

    update ticker 2

--- src/data/tickers.py
+++ src/data/tickers.py
@@ -1,10 +1,10 @@
 ticketer1 = [
     "WA Belongs to Jesus, Not the Democrats",
     "Flip Washington",
-    "Make WA american Again",
-    "This father's day weekend become a founding father of a new revolution",
-    "An American Revelotion founded america. An American reformation will save it.",
-    "An American congress of christian men",
+    "Make WA American Again",
+    "This Father's Day weekend become a founding father of a new revolution",
+    "An American Revolution founded America. An American reformation will save it.",
+    "An American Congress of Christian men",
     "250th Anniversary of the United States"
 ]
 
