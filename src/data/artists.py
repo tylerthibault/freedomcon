d9e2a722bab7a62cea0c6a705b56ee490dc170d9
Tyler <tyler.t@GCC-TylerT.local>
2026-04-18 00:09:55 -0700

    spelling fix from chaz

--- src/data/artists.py
+++ src/data/artists.py
@@ -7,7 +7,7 @@ artists = [
         "genre": "Worship / Folk Rock",
         "bio": """Crowder, GRAMMY-nominated artist with over 3 million records sold and 1.8 billion global streams, is bringing his signature vintage Southern rock to the men of FREEDOM CON on Saturday night.
 
-        As the sun sets over the Columbia River behind the iconic Gorge Amphitheater stage, Crowder's powerhouse sound will close out a perfect weekend with an unforgettable, high-energy summer concert.
+        As the sun sets over the Columbia River behind the iconic Gorge Amphitheatre stage, Crowder's powerhouse sound will close out a perfect weekend with an unforgettable, high-energy summer concert.
 
         Expect soul-stirring anthems and a night that will echo long after the final note fades.""",
         "day": "Saturday",
