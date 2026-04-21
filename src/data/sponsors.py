681dc7d6e25a4a350e71c892d3501f9ad8004422
Tyler <tyler.t@GCC-TylerT.local>
2026-04-21 12:06:24 -0700

    added logos with links

---
+++ src/data/sponsors.py
@@ -0,0 +1,97 @@
+sponsors = {
+    'businesses': [
+            {
+                'name': 'Smith Excavation',
+                'logo_url': '/static/img/sponsor_logos/Smith-Excavation-Logo.svg',
+                'website_url': 'https://www.smithexcavation.com/',
+                'show_on_sponsor_page': True,
+                'background_color': '#000000'
+            },
+            {
+                'name': 'Firefly',
+                'logo_url': '/static/img/sponsor_logos/Firefly_logo.webp',
+                'website_url': 'https://www.thinkfirefly.com/',
+                'show_on_sponsor_page': True,
+                'background_color': "#FFFFFF"
+            },
+            {
+                'name': 'Speiro',
+                'logo_url': '/static/img/sponsor_logos/Speiro_logo.webp',
+                'website_url': 'https://www.gospeiro.com/',
+                'show_on_sponsor_page': True,
+                'background_color': "#FFFFFF"
+            },
+            {
+                'name': 'KBI',
+                'logo_url': 'https://res.cloudinary.com/dzj8q3l 9c/image/upload/v1700000000/sponsors/kbi.png',
+                'website_url': 'https://www.kbi.com/',
+                'show_on_sponsor_page': False
+            },
+            {
+                'name': 'Java Dog',
+                'logo_url': 'https://res.cloudinary.com/dzj8q3l9c/image/upload/v1700000000/sponsors/java_dog.png',
+                'website_url': 'https://www.javadog.com/',
+                'show_on_sponsor_page': False
+            }
+    ],
+    'ministries': [
+            {
+                'name': 'Real Faith',
+                'logo_url': 'https://res.cloudinary.com/dzj8q3l9c/image/upload/v1700000000/sponsors/real_faith.png',
+                'website_url': 'https://www.realfaith.com/',
+                'show_on_sponsor_page': False
+            },
+            {
+                'name': 'Warrior Poet Society',
+                'logo_url': '/static/img/sponsor_logos/wps_logo.png',
+                'website_url': 'https://www.warriorpoetsociety.com/',
+                'show_on_sponsor_page': True,
+                'background_color': "#000000"
+            },
+            {
+                'name': 'Stronger Man Nation',
+                'logo_url': '/static/img/sponsor_logos/SMN_logo.avif',
+                'website_url': 'https://www.strongermannation.com/',
+                'show_on_sponsor_page': True,
+                'background_color': '#000000'
+            }
+    ],
+    'churches': [
+            {
+                'name': 'Pursuit Church',
+                'logo_url': 'https://res.cloudinary.com/dzj8q3l9c/image/upload/v1700000000/sponsors/pursuit_church.png',
+                'website_url': 'https://www.pursuitchurch.com/',
+                'show_on_sponsor_page': False
+            },
+            {
+                'name': 'Grace City Church',
+                'logo_url': 'https://res.cloudinary.com/dzj8q3l9c/image/upload/v1700000000/sponsors/grace_city_church.png',
+                'website_url': 'https://www.gracecitychurch.com/',
+                'show_on_sponsor_page': False
+            },
+            {
+                'name': 'Sage Hills Church',
+                'logo_url': 'https://res.cloudinary.com/dzj8q3l9c/image/upload/v1700000000/sponsors/sage_hills_church.png',
+                'website_url': 'https://www.sagehillschurch.com/',
+                'show_on_sponsor_page': False
+            }
+    ]
+}
+
+
+# Businesses 
+# Smith Excavation
+# Firefly 
+# Speiro 
+# KBI
+# Java Dog 
+
+# Ministries 
+# Real Faith
+# Warrior Poet Society 
+# Stronger Man Nation
+
+# Churches 
+# Pursuit Church 
+# Grace City Church 
+# Sage Hills (check with me first)
