2f5c35a0575731d385b281e49ad4a1aa3ed827c9
Tyler <tyler.t@GCC-TylerT.local>
2026-04-21 15:39:37 -0700

    added our past conferences

---
+++ src/data/about_smn.py
@@ -0,0 +1,103 @@
+about_smn_conferences = [
+	{
+		"year": 2025,
+		"name": "The Brotherhood",
+		"summary": "",
+		"videos": [
+			{
+				"url": "https://www.youtube.com/watch?v=c_N-rVMVNig&list=PLptTyphw8iGNn3CP6uMGliJIU3ZwXieqZ&index=1&pp=iAQB",
+				"title": "Everything has Led to This - Josh McPherson",
+				# "thumbnail_mobile": "img/videos/george_washington_story-mobile.jpg",
+                'mobile_image_x': 50,
+			},
+			{
+				"url": "https://www.youtube.com/watch?v=fID_3ZcIpdE&list=PLptTyphw8iGNn3CP6uMGliJIU3ZwXieqZ&index=2&pp=iAQB",
+				"title": "Finding the True Brotherhood - Shaun's Testimony",
+				# "thumbnail_mobile": "img/videos/to_every_man-mobile.png",
+                'mobile_image_x': -30,
+			},
+			{
+				"url": "https://www.youtube.com/watch?v=d6tVsdFX9lQ&list=PLptTyphw8iGNn3CP6uMGliJIU3ZwXieqZ&index=3&pp=iAQB",
+				"title": "The Immortal Band of Brothers pt. 1 - John Lovell",
+				# "thumbnail_mobile": "img/videos/our_home-mobile.jpg",
+                'mobile_image_x': -45,
+			},
+			{
+				"url": "https://www.youtube.com/watch?v=gI2cXevRzwc&list=PLptTyphw8iGNn3CP6uMGliJIU3ZwXieqZ&index=4&pp=iAQB",
+				"title": "The Immortal Band of Brothers pt. 2 - John Lovell",
+				# "thumbnail_mobile": "img/videos/Well_See_You_at_Freedom_Con-mobile.png",
+                'mobile_image_x': 40,
+			},
+			{
+				"url": "https://www.youtube.com/watch?v=qOYUt_vAI_g&list=PLptTyphw8iGNn3CP6uMGliJIU3ZwXieqZ&index=5&pp=iAQB",
+				"title": "My Brother’s Keeper - Adam James",
+                'mobile_image_x': -40,
+			},
+			{
+				"url": "https://www.youtube.com/watch?v=itWOaG93U9Q&list=PLptTyphw8iGNn3CP6uMGliJIU3ZwXieqZ&index=6&pp=iAQB",
+				"title": "Chasing Brotherhood in the Wrong Place - Brent Frank",
+                'mobile_image_x': -40,
+			},
+		],
+	},
+	{
+		"year": 2024,
+		"name": "Into The Breach",
+		"summary": "Men were strengthened to stand firm in truth, reject passivity, and shoulder responsibility.",
+		"videos": [
+            {
+				"url": "https://www.youtube.com/watch?v=7YQbhEtGPac&list=PLptTyphw8iGNT2YDcnvE4DhSYpqGnVqVQ&index=1",
+				"title": "Be Strong To Win (ft. John Lovell)",
+				# "thumbnail_mobile": "img/videos/george_washington_story-mobile.jpg",
+			},
+			{
+				"url": "https://www.youtube.com/watch?v=0nN8OWTI_E0&list=PLptTyphw8iGNT2YDcnvE4DhSYpqGnVqVQ&index=2",
+				"title": "Be Brave To Fight (ft. John Lovell)",
+				# "thumbnail_mobile": "img/videos/to_every_man-mobile.png",
+			},
+			{
+				"url": "https://www.youtube.com/watch?v=IYFSwPhglBw&list=PLptTyphw8iGNT2YDcnvE4DhSYpqGnVqVQ&index=3",
+				"title": "Be Filled With Power (ft. Mark Driscoll)",
+				# "thumbnail_mobile": "img/videos/our_home-mobile.jpg",
+                'mobile_image_x': -50,
+			},
+			{
+				"url": "https://www.youtube.com/watch?v=Hyo06G9CWH4&list=PLptTyphw8iGNT2YDcnvE4DhSYpqGnVqVQ&index=4",
+				"title": "Be First to Battle | Josh McPherson",
+				# "thumbnail_mobile": "img/videos/Well_See_You_at_Freedom_Con-mobile.png",
+			},
+			{
+				"url": "https://www.youtube.com/watch?v=lqEMqd9QTXw&list=PLptTyphw8iGNT2YDcnvE4DhSYpqGnVqVQ&index=5",
+				"title": "Q&A with John Lovell",
+                'mobile_image_x': -45,
+			},
+		],
+	},
+	{
+		"year": 2023,
+		"name": "Battle Ready",
+		"summary": "Fathers and sons were challenged to build a multi-generational legacy shaped by Scripture.",
+		"videos": [
+            {
+				"url": "https://www.youtube.com/watch?v=1teJXqCs54w&list=PLptTyphw8iGPNogYZNKN4ZTUfAkjkxFPQ&index=1",
+				"title": "Soldier | Stronger Man Nation | Battle Ready",
+				# "thumbnail_mobile": "img/videos/to_every_man-mobile.png",
+                
+			},
+			{
+				"url": "https://www.youtube.com/watch?v=1KicF3p73TA&list=PLptTyphw8iGPNogYZNKN4ZTUfAkjkxFPQ&index=2",
+				"title": "Farmer | Stronger Man Nation | Battle Ready",
+				# "thumbnail_mobile": "img/videos/our_home-mobile.jpg",
+			},
+			{
+				"url": "https://www.youtube.com/watch?v=Q0hB_LpUN7Y&list=PLptTyphw8iGPNogYZNKN4ZTUfAkjkxFPQ&index=3",
+				"title": "Athlete | Stronger Man Nation | Battle Ready",
+				# "thumbnail_mobile": "img/videos/Well_See_You_at_Freedom_Con-mobile.png",
+			},
+			{
+				"url": "https://www.youtube.com/watch?v=6nNLtiIkGUw&list=PLptTyphw8iGPNogYZNKN4ZTUfAkjkxFPQ&index=4",
+				"title": "Son | Stronger Man Nation | Battle Ready",
+			},
+		],
+	},
+]
