c791fbe898290bccb428fb247a0199ea4c5cf209
Tyler <tyler.t@GCC-TylerT.local>
2026-04-22 15:53:40 -0700

    update with podcasts

--- src/data/podcasts.py
+++ src/data/podcasts.py
@@ -7,4 +7,12 @@ podcasts = [
 		"url": "https://www.youtube.com/watch?v=yGNE-3O-EvA&list=PLptTyphw8iGOD-blaMBClVwghZgixDjQg&index=2",
 		"title": "Podcast Episode 2",
 	},
+	{
+		"url": "https://www.youtube.com/watch?v=RImmAJ18RJU",
+		"title": "What Most Couples Get Wrong About Marriage",
+	},
+	{
+		"url": "https://www.youtube.com/watch?v=FMWMV9EBTB4",
+		"title": "The Freedom Conference Matters for Today | Road To Freedom Con Ep. 3",
+	},
 ]
