b5aa8349145e38d85a2a18f7219ca84b479766c9
Tyler <tyler.t@GCC-TylerT.local>
2026-04-24 14:35:05 -0700

    updated podcast mobile images

--- src/data/podcasts.py
+++ src/data/podcasts.py
@@ -2,16 +2,16 @@ podcasts = [
 	{
 		"url": "https://www.youtube.com/watch?v=yGNE-3O-EvA&list=PLptTyphw8iGOD-blaMBClVwghZgixDjQg&index=2",
 		"title": "Podcast Episode 1",
-		"thumbnail_mobile": "img/podcast_thumbnails/podcast_ep1_mobile.avif",
+		"thumbnail_mobile": "img/videos/WE CAN FLIP WASHINGTON STATE (Instagram Story).png",
 	},
 	{
 		"url": "https://www.youtube.com/watch?v=mER8SGz_RiY&list=PLptTyphw8iGOD-blaMBClVwghZgixDjQg&index=1",
 		"title": "Podcast Episode 2",
-		"thumbnail_mobile": "img/podcast_thumbnails/podcast_ep2_mobile.avif",
+		"thumbnail_mobile": "img/videos/WINNING BACK (Instagram Story) - 1.png",
 	},
 	{
 		"url": "https://www.youtube.com/watch?v=FMWMV9EBTB4",
 		"title": "The Freedom Conference Matters for Today | Road To Freedom Con Ep. 3",
-		"thumbnail_mobile": "img/podcast_thumbnails/podcast_ep4_mobile.avif",
+		"thumbnail_mobile": "img/videos/A Vision for Young Men (Instagram Story).jpeg",
 	},
 ]
