835811e785b5d8707c64792e64ce4cf4f5b06ef6
Tyler <tyler.t@GCC-TylerT.local>
2026-04-25 17:19:37 -0700

    added media download py file

---
+++ src/data/media_downloads.py
@@ -0,0 +1,30 @@
+speakers = [
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/1920x1080%20Featured%20Speakers%20(1).jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/1920x1080%20Featured%20Speakers%20VERTICAL%20(1).jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/1920x1080%20GEN%20Z%20Featured%20Speakers%20(1).jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/1920x1080%20GEN%20Z%20Featured%20Speakers%20VERTICAL.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/8.5x11%20Featured%20Speakers%20PLUS%20GEN%20Z.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/SQUARE%20Featured%20Speakers%20(1).jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/SQUARE%20GEN%20Z%20Featured%20Speakers.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/Adam%20James_Flip.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/Chad%20Robichaux_rising.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/Dave%20Barton_freedom.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/Eric%20Metaxas_equipped.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/Graham%20Allen_where.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/John%20Lovell_statesman.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/Josh%20Howerton_freedom.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/Josh%20McPherson_we_stay.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/Mark%20Driscoll_lock.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/Nate%20Schatzline_WA.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/Russell%20Johnson_join.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/Ryan%20Visconti_obey.jpg",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/Time%20Barton_PNW.jpg",
+
+    ]
+
+posters = [
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/Make%20Washington%20America%20Again.png",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/Save%20The%20West%201.png",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/WA_america_again.png",
+    "https://pub-fc470c82f793409f9e6c126deeb0387d.r2.dev/media_download/We%20Will%20Have%20Our%20Home%20Again%202.png"
+]
